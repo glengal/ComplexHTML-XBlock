7649be7297c1d9a957f99c2680ad763618dde316
glengal <glengal88@gmail.com>
2015-08-26 14:21:31 -0400

    wip

--- complexhtml/static/js/complexhtml_lms_chunk_2.js
+++ complexhtml/static/js/complexhtml_lms_chunk_2.js
@@ -1,16 +1,29 @@
-
+console.log("complex js chunk2");
 /* Page is loaded. Do something. */
 $(function ($) {
+    console.log("on page load from chunk2");
     getCleanBody(function(){
         console.log("Before");
-        accessdata();
         console.log("After");
-        anySlide = new AVIATION.common.Slide;
+        anySlide = new AVIATION.common.Slide();
         anySlide.constructor(json_clean_setting);
+        console.log("JSON");
+        console.log(json_clean_setting);
         json_clean_setting.parentSlide = anySlide;
         $(anySlide).on("completedQuiz", function(selectedAnswers){
-    		var checked = $(".answers input:checked");
-            checkQuizResult(checked);
+    		$(".answers input:checked").each(function(index, value){
+            console.log('wow');
+            console.log($(this).attr('id'));
+            checked = $(this).attr('id');
+            });
+            console.log("checked");
+            console.log(checked);
+            var sel = checked.split('_');
+            var selected = sel[3];
+
+	    console.log("Got");
+        checkQuizResult(checked,selected);
+	    sendEmail();
         });
     });
 
