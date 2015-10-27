477b56dea6f38234c23da02d6a479f94c69c7610
Paul Kuzhel <pavlo.kuzhel@gmail.com>
2015-10-27 11:27:55 -0400

    adding pattern support for complexhtml along with patternId

--- complexhtml/static/js/complexhtml_lms_chunk_2.js
+++ complexhtml/static/js/complexhtml_lms_chunk_2.js
@@ -1,8 +1,8 @@
 console.log("complex js chunk2");
 /* Page is loaded. Do something. */
 $(function ($) {
-
     console.log("on page load from chunk2");
+
     getCleanBody(function(){
         console.log("Before");
         console.log("After");
@@ -11,38 +11,38 @@ $(function ($) {
         console.log("JSON");
         console.log(json_clean_setting);
         json_clean_setting.parentSlide = anySlide;
-        $(anySlide).on("completedQuiz", function(selectedAnswers){
-    		$(".answers input:checked").each(function(index, value){
-            console.log('wow');
-            console.log($(this).attr('id'));
-            checked = $(this).attr('id');
-            });
-            console.log("checked");
-            console.log(checked);
-            var sel = checked.split('_');
-            var selected = sel[3];
+        $(anySlide).on("completedQuiz", function(e,type,id){
+            if(type ===  "pattern"){
+                // id variable is now the id of the pattern only
+                // otherwise should all be quiz related
+            } else {
+                $(".answers input:checked").each(function(index, value){
+                    console.log('wow');
+                    console.log($(this).attr('id'));
+                    checked = $(this).attr('id');
+                });
+                console.log("checked");
+                console.log(checked);
+                var sel = checked.split('_');
+                var selected = sel[3];
 
-	    console.log("Got");
-        checkQuizResult(checked,selected);
-	    sendEmail();
+                console.log("Got");
+                checkQuizResult(checked,selected);
+                sendEmail();
+            }
         });
     });
 
-if ($(".action-publish") === undefined) {
-
-    session_start();
-
-    tick_timer = setInterval(function () {
-
+    if($(".action-publish") === undefined){
+        session_start();
+        tick_timer = setInterval(function (){
         $.ajax({
             type: "POST",
             url: runtime.handlerUrl(xblock_element, 'session_tick'),
             data: JSON.stringify({}),
             async: false
         });
-
     }, session_tick);
-
 }
 
 $(window).unload(function() { session_end(); });
